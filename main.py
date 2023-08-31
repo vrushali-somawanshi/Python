9b3b386eb5e1d786f3e5e5ab80d8e32c4b91e7cb
vrushali-somawanshi <vrushalisomawanshi@gmail.com>
2023-08-31 14:46:40 +0530

    2:print statement use 4,5: Value assigned to variable 9to12: type of data type show using type keyword 14,15 : if statement use for indentation(4 times whitespace for print keyword) 17,18: Assign String to Variable 20,21: Get the character position from 1 to 5 23,24: upper function use 26,27: lower function use 29,30: strip function use for removing any whitespace from beginning or end 32:33: replace function use for replace string from one string to another 35,36: Split function use for split string into substring 38to42: String Concatenation use 44to46: add a space between them, add a " " 48to50: we can combine strings and numbers by using the format() method!places them in the string where the placeholders {} are ---numbers into string 52to56:  format() method takes unlimited number of arguments, and are placed into the respective placeholders 58to63: Boolean operators use 65to66: List use 68to69: Access list 71,72: list(1st two element print# beginning) 74,75:list(end element print) 77to79: Change list item 81 to 83: loop through list 85 to 87: Remove specific item using remove 89 to 91 :Remove specific index item using pop or use del keyword 93to95: if you do not specified index then it remove by default last item using pop 97to98: del keyword use to delete entire list 100to102: using clear keyword list will empty 104to108: float to int conversion 110to120: List comprehension 122to124: using sort alphabet sort ascending by default 126to128: using sort(reverse=true) alphabet sort in descending order 134to138: join list

--- main.py
+++ main.py
@@ -130,3 +130,9 @@ print(fruits)
 fruits = ["orange", "mango", "kiwi", "pineapple", "banana"]
 newlist = fruits.copy()
 print(fruits)
+
+list1 = ["v", "r", "u", "s", "h"]
+list2 = [1, 2, 3,4,5,6,7,8,9]
+
+list3 = list1 + list2
+print(list3)
