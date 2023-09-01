536c5c27616e0610c1aff5e440e4e13c689cdd12
vrushali-somawanshi <vrushalisomawanshi@gmail.com>
2023-09-01 15:34:01 +0530

    2:print statement use 4,5: Value assigned to variable 9to12: type of data type show using type keyword 14,15 : if statement use for indentation(4 times whitespace for print keyword) 17,18: Assign String to Variable 20,21: Get the character position from 1 to 5 23,24: upper function use 26,27: lower function use 29,30: strip function use for removing any whitespace from beginning or end 32:33: replace function use for replace string from one string to another 35,36: Split function use for split string into substring 38to42: String Concatenation use 44to46: add a space between them, add a " " 48to50: we can combine strings and numbers by using the format() method!places them in the string where the placeholders {} are ---numbers into string 52to56:  format() method takes unlimited number of arguments, and are placed into the respective placeholders 58to63: Boolean operators use 65to66: List use 68to69: Access list 71,72: list(1st two element print# beginning) 74,75:list(end element print) 77to79: Change list item 81 to 83: loop through list 85 to 87: Remove specific item using remove 89 to 91 :Remove specific index item using pop or use del keyword 93to95: if you do not specified index then it remove by default last item using pop 97to98: del keyword use to delete entire list 100to102: using clear keyword list will empty 104to108: float to int conversion 110to120: List comprehension 122to124: using sort alphabet sort ascending by default 126to128: using sort(reverse=true) alphabet sort in descending order 134to138: join list 140,141: Tuple 143,144: Access Tuple 146,149: Change Tuple Values(Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
    But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.)
    
    You cannot access items in a set by referring to an index or a key.
    
    156to158:But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
    160to161:Check if "banana" is present in the set:
    163to168: Dictionary Create
    170to176: Access dictionary items
    178to181: while loop
    183to188:  the break statement we can stop the loop even if the while condition is true:
    190to195: continue statement we can stop the current iteration, and continue with the next
    197to200: Function defination and function Calling

--- main.py
+++ main.py
@@ -180,3 +180,22 @@ while i < 8:
     print(i)
     i += 1
 
+i = 1
+while i > 8:
+    print(i)
+    if i == 6:
+        break
+        i += 1
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+def my_fruit():
+    print("Hello I'm from fruit function")
+
+my_fruit()
+
