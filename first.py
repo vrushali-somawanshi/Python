d07fdc61e69e0aaa6bf6b3a4b9a3da55f7a4d582
vrushali-somawanshi <vrushalisomawanshi@gmail.com>
2025-04-25 15:36:48 +0530

    update changes

--- first.py
+++ first.py
@@ -24,14 +24,35 @@ arr = np.array(list)
 print(arr)
 
 #numpy methods
-arr = np.arange(0,9)    #numpy method(arange -> start from 0 and end with 8 i.e last number is not consider)
-arr = np.arange(0,11,2)  #arange all even number between 0 to 11 
+arr1 = np.arange(0,9)    #numpy method(arange -> start from 0 and end with 8 i.e last number is not consider)
+arr2 = np.arange(0,11,2)  #arange all even number between 0 to 11 
+print(arr1)
+print(arr2)
 
-arr = np.zeros(1)   # numpy methodzeros-> print in output one zero
-arr = np.zeros(4)   #print in output 4 zero
-arr = np.zeros((3,5))   # print in output 3 rows and 5 column zeros
+arr3 = np.zeros(1)   # numpy method zeros-> print in output one zero
+arr4 = np.zeros(4)   #print in output 4 zero
+arr5 = np.zeros((3,5))   # print in output 3 rows and 5 column zeros
+print(arr3)
+print(arr4)
+print(arr5)
 
-print(arr)
 
+arr6 = np.ones(3)   # numpy ones method print in output 4 ones
+arr7 = np.ones((4,5))  #print in output 4 row and 5 column ones
+print(arr6)
+print(arr7)
+
+arr8 = np.linspace(0,5,10)  #  numpy linspace start with 0 and end with 5 and getting 10 evenly point in between 0 to 5
+print(arr8)     
+
+arr9 = np.eye(4)  # numpy eye method  print 2-d array with identity matrices
+print(arr9)
+
+import numpy as np       # create an array using numpy arange method
+arrr = np.arange(0,11)    # start with 0 and end with 10
+print(arrr)
+
+arrrr = np.array([0,1, 2, 3, 4,5,6,7,8,9,10])
+print(arrrr[1:5])    # array indexing with slice method
 
 
